971f2a63c1a36b295cea3787282667e372ccb57d
smeagol <smeagol@gmail.com>
2017-04-05 17:04:37 +0000

    invoke the 'npm install -g sm' sequence followed by 'npm install @angular/animations --save' to make 'animations' work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,33 @@
 import { 
-  Component,trigger,state,style,transition,animate,keyframes 
+    Component,trigger,state,style,transition,animate,keyframes
 } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `<button [@myTrigger]='state'>Respect HTML</button>`,
-  styles: [],
-  animations: [
-    trigger('myTrigger',[
-      //state()
-      //transition()
-    ])
-  ]
+    selector: 'app-root',
+    template: `<button [@myTrigger]='state' (click)='toggleState()'>Respect HTML</button>`,
+    styles: [],
+    animations: [
+        trigger('myTrigger',[
+
+            state('small',style({
+                backgroundColor: '#cfd8dc',
+                transform: 'scale(1)'
+            })),
+
+            state('large',style({
+                backgroundColor: '#ff0000',
+                transform: 'scale(5.4)'
+            })),
+
+            //transition('large <=> small',animate('100ms'))
+        ])
+    ]
 })
 export class AppComponent {
-  state: string = "small";
+    state: string = 'small';
+
+    toggleState() {
+        this.state = (this.state == 'small' ? 'large': 'small');
+        console.log("toggle state = " + this.state);
+    }
 }
