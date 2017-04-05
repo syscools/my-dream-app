03400b0964d86618c4367bf22e574c89332e2ed4
smeagol <smeagol@gmail.com>
2017-04-05 19:30:39 +0000

    animation timing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,23 @@ import {
 
 @Component({
     selector: 'app-root',
+
     template: `<button (click)='toggleState()'>Respect HTML</button>
         <ul>
             <li *ngFor="let item of items" [@myTrigger]='state'>{{ item }}</li>
         </ul>`,
+
     styles: [`
-        ul { list-style-type:node;margin:30px 30px 0 0;padding:0;}
+        ul { list-style-type:node; margin:30px 30px 0 0; padding:0;}
         li {
-            padding:15px;
-            width:100%;
-            background:#f1f1f1;
-            margin-bottom:2px;
-            font-weight:bold;
+            padding: 15px;
+            width: 100%;
+            background: #f1f1f1;
+            margin-bottom: 2px;
+            font-weight: bold;
         }
     `],
+
     animations: [
         trigger('myTrigger',[
 
@@ -40,23 +43,32 @@ import {
                 opacity: '1'
             })),
               
-            //transition('* => *',animate('500ms ease-in'))
+            //transition('* => *',animate('5000ms ease-in'))
+
+            transition('void => *',[
+                style({ opacity: '0', transform: 'translateY(20px)' }),
+                animate('500ms')
+            ])
+
+            /*
             transition('void => *',[
                 animate('500',keyframes([
                     style({opacity:0, transform: 'translateY(-30px)',offset:0}),
-                    style({opacity:1, transform: 'translateY(5px)',offset:0.3}),
+                    style({opacity:1, transform: 'translateY(5px) scale(1.2)',offset:0.3}),
                     style({opacity:1, transform: 'translateY(0)',offset:1})
                 ]))
             ])
+            */
         ])
     ]
 })
+
 export class AppComponent {
     state: string = 'extra-large';
     items = [];
 
     toggleState() {
-        this.items.push('idiot : ' + this.state);
+        this.items.push('apple : ' + this.state);
         this.state = "fadeIn";    
     }
 }
