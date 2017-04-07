09033ceda81a16832634c16c83d462c49f91908d
smeagol <smeagol@gmail.com>
2017-04-07 04:22:32 +0000

    todo: Find best way to change color of one <li> only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,47 +4,60 @@ import {
 
 @Component({
     selector: 'app-root',
-    template: `<button (click)='toggleState()'>Respect HTML</button>
-        <ul>
-            <li *ngFor="let item of items" [@myTrigger]='state'>{{ item }}</li>
+    template: `<button (click)='toggleState()'>click me</button>
+        <h3 [@myTrigger]='state'>A series of random numbers</h3>
+        <ul >
+            <li *ngFor="let item of items" [@myTrigger]='state'><span >{{ item }}</span></li>
         </ul>`,
-    styles: [],
+    styles: [`
+        li {
+            background: #f1f1f1;
+        }
+    `],
     animations: [
         trigger('myTrigger',[
-
-            state('small',style({
-                backgroundColor: '#cfd8dc',
-                transform: 'scale(1)'
-            })),
-
-            state('large',style({
-                backgroundColor: '#ff0000',
-                transform: 'scale(5.4)'
-            })),
-
-            state('extra-large',style({
-                backgroundColor: '#ff0000',
-                transform: 'scale(5.4)'
+    
+            state('state3',style({
+                background: '#ff0000'
             })),
               
-            state('fadeIn',style({
-                opacity: '1'
+            state('state1',style({
+                background: '#00ff00'
             })),
-              
+
             //transition('* => *',animate('500ms ease-in'))
-            transition('void => *',[
+
+            /*
+            transition('* => *',[
+                style({ transform: 'scale(2)' })
+            ]),
+            */
+           
+            /* 
+            transition('* => *',[
                 style({ opacity: '0',transform:'translateY(20px)' }),
                 animate('500ms')
             ])
+            */
         ])
     ]
 })
+
 export class AppComponent {
-    state: string = 'extra-large';
+
+    state: string = 'unknown';
     items = [];
+    state_values = ['state1','state2','state3','state4','state5'];
+
+    constructor() { 
+
+    }
 
     toggleState() {
-        this.items.push('idiot : ' + this.state);
-        this.state = "fadeIn";    
+        var r = Math.floor(Math.random() * this.state_values.length);
+        var q = Math.random();
+        this.state = this.state_values[r];
+        console.log(this.state);
+        this.items.push(q.toString()); 
     }
 }
